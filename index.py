53bd721cbd9b6b559e13d63bf3bdd032efca2e69
lilleKrille <krigus@duck.com>
2023-09-18 18:19:29 +0200

    Update index.py

--- index.py
+++ index.py
@@ -1,3 +1,35 @@
+# g(x,y)
+# reiknar ut kva trenden i eit datasett er. 
+# Parameter: List X, Y
+# kommentert ut kode som eg ikkje ser at me har bruk for
+def g(x,y):
+    teller, nevner = 0, 0
+    snittX = snitt(x)
+    snittY = snitt(y)
+    for i in range(len(x)):
+        teller += (x[i]-snittX)*(y[i]-snittY)
+        nevner += (x[i]-snittX)**2
+    a = teller/nevner
+#    b = snittY - a*snittX
+    return a#, b
+
+#reiknar snittet til ei liste
+def snitt(liste):
+    snitt = 0
+    for item in liste:
+        snitt += item
+    return snitt/len(liste)
+
+def k(x,y):
+    a = g(x,y)
+    if a < 0:   print("trenden er negativ")
+    elif a > 0: print("trenden er positiv")
+    else:       print("ingen trend")
+
+def i(temperaturliste):
+    typeDager = [0,0,0]
+    print(f"sommerdager: {list[0]} høysommerdager: {list[1]} tropedager: {list[2]}")
+
 #Listene som ble oppgitt i oppgaven
 temperaturer = [-5, 2, 6, 13, 9, 22, 28, 19, 24, 12, 5, 1, -3, -8, 2, 8, 15, 18, 21, 26, 21, 31, 15, 4, 1, -2]
 dogn_nedbor = [2, 5, 0, 0, 0, 3, 6, 4, 0, 0, 5, 0, 12, 12, 12, 12, 7, 19]
@@ -30,3 +62,13 @@ def lengdeLengsteSekvens(aListe, aVerdi = 0):
 
 #Oppgave m) Bruk funksjonen fra oppgave f) på den oppgitte lista «døgn-nedbør» for å finne den lengste perioden uten nedbør
 print(f"Den lengste perioden uten nedbør varte i {lengdeLengsteSekvens(dogn_nedbor)} dager")
+
+temperaturer = [-5, 2, 6, 13, 9, 22, 28, 19, 24, 12, 5, 1, -3, -8, 2, 8, 15, 18,
+21, 26, 21, 31, 15, 4, 1, -2]
+dogn_nedbor = [2, 5, 0, 0, 0, 3, 6, 4, 0, 0, 5, 0, 12, 12, 12, 12, 7, 19]
+temperaturer_tidspunkter = list()
+for index in range(len(temperaturer)):
+    temperaturer_tidspunkter.append(index)
+    
+for i in range(20,5,30):
+    print(i)
