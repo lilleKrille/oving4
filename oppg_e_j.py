54c875db105914d2a122eab5354382ec79d3af5a
Anders Bygdås <andbygd@gmail.com>
2023-09-22 15:59:14 +0200

    ferdig med e, j

--- oppg_e_j.py
+++ oppg_e_j.py
@@ -10,3 +10,30 @@ hvert tidspunkt. Gå gjennom lista som dere får når dere kaller funksjonen fra
 temperaturlista. For hvert element skriv ut indeksen og skriv ut «stigende» om differansen er
 over 0, «synkende» om den er negativ eller «uforandret» om den er 0.
 """
+
+temperaturer = [-5, 2, 6, 13, 9, 22, 28, 19, 24, 12, 5, 1, -3, -8, 2, 8, 15, 18, 21, 26, 21, 31, 15, 4, 1, -2]
+
+# e)
+def beregne_differanser(liste):
+    differanser = []
+    for i in range(1,len(liste)):
+        diff = liste[i]-liste[i-1]
+        differanser.append(diff)
+    return differanser
+
+
+
+diffs = beregne_differanser(temperaturer)
+print(diffs)
+
+#j)
+for index in range(len(diffs)):
+    if diffs[index] > 0:
+        print(f"Stigende for index: {index}")
+    elif diffs[index] < 0:
+        print(f"Synkende for index: {index}")
+    else:
+        print(f"Uforandret for index: {index}")
+
+
+
