6830af69e8acbec4b6afb04f9a4c7bf9cf3dd6d4
Anders Bygdås <andbygd@gmail.com>
2023-09-22 14:43:28 +0200

    start oppgave e+j

---
+++ oppg_e_j.py
@@ -0,0 +1,14 @@
+"""
+
+e)
+Skriv en funksjon som tar inn ei liste med tall. For hvert tall i lista unntatt det siste skal
+funksjonen regne ut differansen mellom neste tall i lista og dette tallet. Differansene skal
+legges inn i ei ny liste
+
+j)
+Bruk funksjonen fra oppgave e) til å finne ut om temperaturen er stigende eller synkende for
+hvert tidspunkt. Gå gjennom lista som dere får når dere kaller funksjonen fra oppgave e) på
+temperaturlista. For hvert element skriv ut indeksen og skriv ut «stigende» om differansen er
+over 0, «synkende» om den er negativ eller «uforandret» om den er 0.
+
+"""
