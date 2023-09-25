1b25af06f7b6063d9f4412dce89d2def9bd7532f
Tord-Svensen <136593835+Tord-Svensen@users.noreply.github.com>
2023-09-25 10:03:05 +0200

    Øving h + l

--- index.py
+++ index.py
@@ -1,1 +1,27 @@
-print("hello")
+
+def sum_over_5 (liste_grader): #Funksjon som finner ut om temperaturene i listen er over 5 for så legge verdi over 5 i sum.
+    sum = 0
+    for temp in liste_grader:
+        if temp > 5:
+            sum += temp - 5
+    return sum
+
+
+liste_grader = [4,5,7,10,15]
+Resultat_over_5 = sum_over_5(liste_grader)
+print(Resultat_over_5)
+
+
+def sum_over_5 (liste_grader): #Funksjon som finner ut om temperaturene i listen er over 5 for så legge verdi over 5 i sum.
+    sum = 0
+    for temp in liste_grader:
+        if temp > 5:
+            sum += temp - 5
+        elif temp < 0: #tar skade ved nagive grader
+            sum += temp     
+    return sum
+
+
+liste_grader = temperaturer
+Resultat_over_5 = sum_over_5(liste_grader)
+print("Dersom vekst er gitt i cm tilsvarere dette: ", Resultat_over_5,"cm")
