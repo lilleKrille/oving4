09127d6e7c0220921af0fd95aba21e539007b5e9
Anders Bygdås <andbygd@gmail.com>
2023-11-14 17:45:09 +0100

    oppg g og i ferdig

--- oving10g.py
+++ oving10g.py
@@ -29,10 +29,10 @@ def lesFraFil(aFilnavn):
 
             snodybde.append(lineData[3])
             nedbor.append(lineData[4])
-            middeltemperatur.append(lineData[5])
-            snittSkydekke.append((lineData[6].replace(",", "."),datoObjekt ))
+            middeltemperatur.append((lineData[5].replace(",", "."), datoObjekt)) # legg til dato i dict, og erstatt , med .
+            snittSkydekke.append((lineData[6].replace(",", "."),datoObjekt )) # legg til dato i dict, og erstatt , med .
             middelvind.append(lineData[7].strip("\n"))
-            
+
     data = dict()
     data["navn"] = navn
     data["stasjon"] = stasjon
@@ -47,25 +47,70 @@ def lesFraFil(aFilnavn):
 
 
 def plot_penvaersdager(datasett):
-    snittskydekke = datasett["snittSkydekke"]
     data = dict()
     #obj is (VALUE, DATE)         obj[0] = VALUE, obj[1] = DATE
-    for obj in snittskydekke:
+    print(datasett)
+    for obj in datasett:
         try:
             if obj[1].year in data:
                 if float(obj[0]) <= 3:
                     data[obj[1].year] = data[obj[1].year] + 1
             else:
                 if float(obj[0]) <= 3:
-                    data[obj[1].year] = 0
-        except ValueError:
+                    data[obj[1].year] = 1
+        except ValueError: 
             continue
 
     plt.plot(list(data.keys()), list(data.values()), "o-")
     plt.show()
 
 
+def beregne_differanser(liste):
+    differanser = []
+    for i in range(1,len(liste)):
+        diff = liste[i]-liste[i-1]
+        differanser.append(diff)
+    return differanser
+{ 
+ "07/2020": [2,3,4,4,5,2,8],
+ "2021": [3,2,5,6,4],
+ "2022": 2,
+ "2023": 1,
+ "1980": 0,
+ }
+[2020, 2021, 2022, 2023, 1980]
+[2,3,2,1,0]
+
+def plot_temp_per_mnd(datasett):
+    result = dict()
+    for data in datasett:
+        try:
+            date = f"{data[1].month}/{data[1].year}"
+            if date in result:
+                result[date].append(float(data[0]))
+            else:
+                result[date] = [float(data[0])]
+        except ValueError:
+            continue
+
+    # calculate average:
+    result_avg = dict()
+    for key in result:
+        result_avg[key] = round(sum(result[key])/len(result[key]), 2)
+    
+    # Finn differanser:
+    result_diff = beregne_differanser(list(result_avg.values()))
+
+    # PLOT:
+    plt.plot(list(result_avg.keys()),list(result_avg.values()), "o-", label="gjennomsnitt temperatur")
+    plt.plot(list(result_avg.keys())[1:],result_diff, "o-", label="gjennomsnitt temperatur")
+    plt.legend()
+    plt.show()
+
+
+
 
 if __name__ == "__main__":
     datasett = lesFraFil("snoedybder_vaer_en_stasjon_dogn.csv")
-    plot_penvaersdager(datasett)
+    plot_penvaersdager(datasett["snittSkydekke"])
+    plot_temp_per_mnd(datasett["middeltemperatur"])
